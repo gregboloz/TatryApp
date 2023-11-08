26739b9f5383468665e05530cbe18821e07809b2
gregboloz <117506426+gregboloz@users.noreply.github.com>
2023-11-08 20:32:21 +0100

    small update

---
+++ app/src/main/java/com/example/tatryapp/EncyklopediaActivity.java
@@ -0,0 +1,14 @@
+package com.example.tatryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EncyklopediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encyklopedia);
+    }
+}
---
+++ app/src/main/java/com/example/tatryapp/MapyActivity.java
@@ -0,0 +1,14 @@
+package com.example.tatryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapy);
+    }
+}
